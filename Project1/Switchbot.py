743e0d7c4f71615aaa23b4e98bf272f1e2d056e7
cychan110 <113387605+cychan110@users.noreply.github.com>
2022-12-09 16:10:59 +0800

    Update Switchbot.py

--- Project1/Switchbot.py
+++ Project1/Switchbot.py
@@ -2,13 +2,17 @@
 import Adafruit_BBIO.PWM as PWM
 import Adafruit_BBIO.GPIO as GPIO
 import time
+import Adafruit_BBIO.ADC as ADC
 
-# Initial Variables
+
+# Initializing pins and variables
 servo1 = "P1_33" #for turning switch on
 servo2 = "P1_36" #for turning switch off
 
 BTinput = 0
 BT = "P1_3" #pin connected to USB breakout board, which has Bluetooth module plugged in
+
+temp = "P1_2" #temperature sensor
 
 # Returning Servos to Neutral Position
 PWM.start(servo1,7.5,50)
@@ -35,13 +39,41 @@ while True:
         PWM.stop(servo1)
         PWM.cleanup()
         
-    else BTinput = 0 #Bluetooth signaling "Off" command     
+        # Getting temperature readout
+        ADC.setup()
+        while True:
+            reading = ADC.read(temp)
+            millivolts = reading * 1800  # 1.8V reference = 1800 mV
+            temp_c = (millivolts - 500) / 10
+            temp_f = (temp_c * 9/5) + 32
+            print('C=%d F=%d' % (temp_c, temp_f))
+            time.sleep(1)
+        
+    elif BTinput = 0 #Bluetooth signaling "Off" command     
         PWM.start(servo2,5,50) #rotating servo2 all the way right
         time.sleep (1.0)
         PWM.start(servo2,7.5,50) #resetting servo2 to neutral
         time.sleep (1.0)
         PWM.stop(servo2)
         PWM.cleanup()
+        # Getting temperature readout
+        ADC.setup()
+        while True:
+            reading = ADC.read(temp)
+            millivolts = reading * 1800  # 1.8V reference = 1800 mV
+            temp_c = (millivolts - 500) / 10
+            temp_f = (temp_c * 9/5) + 32
+            print('C=%d F=%d' % (temp_c, temp_f))
+            time.sleep(1)
             
-    
+    else #standby state
+        PWM.start(servo1,7.5,50)
+        time.sleep (1)
+        PWM.stop(servo1)
+        PWM.cleanup()
+        PWM.start(servo2,7.5,50)
+        time.sleep (1)
+        PWM.stop(servo2)
+        PWM.cleanup()
+        
     # Press CTRL + C to end program
